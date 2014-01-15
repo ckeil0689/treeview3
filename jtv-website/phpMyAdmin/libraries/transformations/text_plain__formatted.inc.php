48b6a205486dc63c06289ea044e7478a42ba0ded
Chris Keil <ckeil@princeton.edu>
2014-01-15 13:04:04 -0500

    deleted files and updated code

--- jtv-website/phpMyAdmin/libraries/transformations/text_plain__formatted.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* $Id: text_plain__formatted.inc.php,v 1.1 2004-12-05 20:16:35 alokito Exp $ */
-// vim: expandtab sw=4 ts=4 sts=4:
-
-function PMA_transformation_text_plain__formatted($buffer, $options = array(), $meta = '') {
-    return $buffer;
-}
-
-?>
--- jtv-website/phpMyAdmin/themes/original/info.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* $Id: info.inc.php,v 1.1 2004-12-05 20:16:35 alokito Exp $ */
-/* Theme information */
-$theme_name = 'Original';
-$theme_version = 1;
-$theme_generation = 1;
-?>
