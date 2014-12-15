7c5375333dbf517a9526051253b27c8d0f733c47
Chris Keil <ckeil0689@gmail.com>
2014-12-15 23:04:26 +0100

    minor adjustment to UrlSettingsPanel

--- LinkedView/src/edu/stanford/genetics/treeview/UrlSettingsPanel.java
+++ LinkedView/src/edu/stanford/genetics/treeview/UrlSettingsPanel.java
@@ -31,7 +31,6 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
