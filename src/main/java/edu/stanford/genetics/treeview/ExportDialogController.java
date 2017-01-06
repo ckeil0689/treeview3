8224ecdf4d5a1a06fcced7c016ce835d3c04dfd9
hepcat72 <hepcat72@gmail.com>
2017-01-06 12:13:57 -0500

    Ran the formatter on a couple classes I intend to work on.  I did mess with the format settings a tad because it has been causing vast amounts of tabs in other places I've seen, sending code off the right of any rational display width.  I will submit a new PR for the changes to the formatter.  Unitl then, I'm fine with formatting getting changed if someone else touches it.  The one weird thing I couldn't figure out was a conditional whose block it unreasonably joined into a single long line - no idea why despite a bunch of setting changes.
    The main issue was that despite setting the indent amount to a single
    tab, individual statement types were set to indent indent "on column",
    which is what was inserting all those insanely long tabs.

--- src/main/java/edu/stanford/genetics/treeview/ExportDialogController.java
+++ src/main/java/edu/stanford/genetics/treeview/ExportDialogController.java
@@ -30,23 +30,23 @@ public class ExportDialogController {
 	private final TreeSelectionI rowSelection;
 	private DataModel model;
 
-	public ExportDialogController(final ExportDialog expD, 
-			final TreeViewFrame tvFrame,final MapContainer interactiveXmap,
-			final MapContainer interactiveYmap,final DataModel model) {
-		
-		this.exportDialog = expD;
+	public ExportDialogController(final ExportDialog expD,
+		final TreeViewFrame tvFrame,final MapContainer interactiveXmap,
+		final MapContainer interactiveYmap,final DataModel model) {
+
+		exportDialog = expD;
 		this.tvFrame = tvFrame;
-		this.dendroView = tvFrame.getDendroView();
+		dendroView = tvFrame.getDendroView();
 		this.interactiveXmap = interactiveXmap;
 		this.interactiveYmap = interactiveYmap;
-		this.colSelection = tvFrame.getColSelection();
-		this.rowSelection = tvFrame.getRowSelection();
+		colSelection = tvFrame.getColSelection();
+		rowSelection = tvFrame.getRowSelection();
 		this.model = model;
-		
-		this.exportOptions = new ExportOptions();
 
-		addListeners();		
-		
+		exportOptions = new ExportOptions();
+
+		addListeners();
+
 		setNewPreviewComponents(exportOptions);
 		exportDialog.arrangePreviewPanel();
 		updatePreview();
@@ -57,34 +57,35 @@ public class ExportDialogController {
 	 * Adds listeners to the GUI components in ExportDialog.
 	 */
 	private void addListeners() {
-		
+
 		exportDialog.addExportListener(new ExportListener());
 		exportDialog.addFormatListener(new FormatListener());
 		exportDialog.addRegionListener(new RegionListener());
 		exportDialog.addItemStateListener(new RadioItemStateListener());
 		exportDialog.addCheckBoxItemStateListener(new CheckItemStateListener());
 	}
-	
+
 	/**
 	 * Set new components for displaying previews of trees and matrix in the
 	 * export dialog.
+	 * 
 	 * @param withSelections - whether to show selections or not
 	 */
 	private void setNewPreviewComponents(final ExportOptions options) {
-		
-		ExportPreviewTrees expRowTrees = 
-				tvFrame.getDendroView().getRowTreeSnapshot(
-						options.isShowSelections(), options.getRegionType());
-		ExportPreviewTrees expColTrees = 
-				tvFrame.getDendroView().getColTreeSnapshot(
-						options.isShowSelections(), options.getRegionType());
-		ExportPreviewMatrix expMatrix = 
-				tvFrame.getDendroView().getMatrixSnapshot(
-						options.isShowSelections(), options.getRegionType());
-		
-		exportDialog.setPreviewComponents(expRowTrees, expColTrees, expMatrix);
+
+		ExportPreviewTrees expRowTrees =
+			tvFrame.getDendroView().getRowTreeSnapshot(
+				options.isShowSelections(),options.getRegionType());
+		ExportPreviewTrees expColTrees =
+			tvFrame.getDendroView().getColTreeSnapshot(
+				options.isShowSelections(),options.getRegionType());
+		ExportPreviewMatrix expMatrix =
+			tvFrame.getDendroView().getMatrixSnapshot(
+				options.isShowSelections(),options.getRegionType());
+
+		exportDialog.setPreviewComponents(expRowTrees,expColTrees,expMatrix);
 	}
-	
+
 	/**
 	 * Describes the actions used to produce an export.
 	 */
@@ -94,47 +95,50 @@ public class ExportDialogController {
 		public void actionPerformed(ActionEvent arg0) {
 
 			//This will open a file chooser dialog
-			String exportFilename = 
-					chooseSaveFile(exportOptions.getFormatType());
+			String exportFilename =
+				chooseSaveFile(exportOptions.getFormatType());
 
 			//If the returned string is null or empty, they either canceled or
 			//there was an error
-			if(exportFilename == null || exportFilename.isEmpty()) {
+			if((exportFilename == null) || exportFilename.isEmpty()) {
 				LogBuffer.println("Could not export. A file name could "
-						+ "not be created.");
+					+ "not be created.");
 				return;
 			}
 
 			//Now export the file
 			try {
-				ExportHandler eh = new ExportHandler(dendroView,interactiveXmap,
+				ExportHandler eh = new ExportHandler(dendroView,
+					interactiveXmap,
 					interactiveYmap,colSelection,rowSelection);
 				//TODO use and pass ExportOptions object instead
 				eh.setDefaultPageSize(exportOptions.getPaperType());
 				eh.setDefaultPageOrientation(exportOptions.getOrientation());
 				eh.setTileAspectRatio(exportOptions.getAspectType());
-				boolean exportSucess = eh.export(exportOptions.getFormatType(), 
-						exportFilename, 
-						exportOptions.getRegionType(), 
-						exportOptions.isShowSelections());
+				boolean exportSucess = eh.export(exportOptions.getFormatType(),
+					exportFilename,
+					exportOptions.getRegionType(),
+					exportOptions.isShowSelections());
 
-				if(exportSucess){
+				if(exportSucess) {
 					String msg = "Exported file: [" + exportFilename + "].";
 					LogBuffer.println(msg);
-	
+
 					exportDialog.dispose();
-	
+
 					//Open the file in the default system app
 					Desktop.getDesktop().open(new File(exportFilename));
-				} else{
-					String msg = "Could not export the file : [" + exportFilename + "].";
+				} else {
+					String msg = "Could not export the file : [" +
+						exportFilename + "].";
 					LogBuffer.println(msg);
 				}
-			} catch(Exception iae) {
+			}
+			catch(Exception iae) {
 				LogBuffer.logException(iae);
 				showWarning(iae.getLocalizedMessage());
 			}
-	    }
+		}
 	}
 
 	public String chooseSaveFile(FormatType selFormat) {
@@ -152,8 +156,9 @@ public class ExportDialogController {
 			File outFile = new File(getInitialExportFileString(selFormat));
 			fileDialog.setDirectory(inFile.getParentFile().getCanonicalPath());
 			fileDialog.setFile(outFile.getName());
-			
-		} catch(Exception e) {
+
+		}
+		catch(Exception e) {
 			e.printStackTrace();
 		}
 
@@ -162,7 +167,7 @@ public class ExportDialogController {
 		//Retrieve the chosen output file
 		final String outdir = fileDialog.getDirectory();
 		final String filename = fileDialog.getFile();
-		if(outdir != null && filename != null) {
+		if((outdir != null) && (filename != null)) {
 			chosen = outdir + filename;
 		}
 
@@ -171,7 +176,7 @@ public class ExportDialogController {
 
 	public String getInitialExportFileString(FormatType selFormat) {
 		String exportFilename = model.getSource();
-		if(exportFilename == null || exportFilename.isEmpty()) {
+		if((exportFilename == null) || exportFilename.isEmpty()) {
 			exportFilename = "TreeView3_exported_file";
 		}
 		exportFilename += "." + selFormat.toString();
@@ -180,13 +185,13 @@ public class ExportDialogController {
 
 	private void showWarning(final String message) {
 
-		JOptionPane.showMessageDialog(tvFrame.getAppFrame(), 
-				message, "Warning", JOptionPane.WARNING_MESSAGE);
+		JOptionPane.showMessageDialog(tvFrame.getAppFrame(),
+			message,"Warning",JOptionPane.WARNING_MESSAGE);
 		LogBuffer.println(message);
 	}
 
 	/**
-	 * 
+	 *
 	 *
 	 */
 	private class FormatListener implements ActionListener {
@@ -203,13 +208,13 @@ public class ExportDialogController {
 			ExportHandler eh = new ExportHandler(tvFrame.getDendroView(),
 				interactiveXmap,interactiveYmap,tvFrame.getColSelection(),
 				tvFrame.getRowSelection());
-			
+
 			List<RegionType> tooBigs = new ArrayList<RegionType>();
 			if(!selFormat.isDocumentFormat()) {
 				final boolean useMinimums = true;
 				tooBigs = eh.getOversizedRegions(useMinimums);
 			}
-			
+
 			exportDialog.setBigRegs(tooBigs);
 			exportDialog.updateRegionRadioBtns(selFormat.isDocumentFormat());
 		}
@@ -227,63 +232,63 @@ public class ExportDialogController {
 				selRegion);
 		}
 	}
-	
+
 	/**
 	 * Whenever the selection state of any GUI component which has this listener
 	 * attached will be changed, it causes the export preview to update.
-	 * For example, if the user checks the 'Show selections' JCheckBox, 
+	 * For example, if the user checks the 'Show selections' JCheckBox,
 	 * the update will invoke the recreation of the preview components so
-	 * that selections will be drawn as well. 
+	 * that selections will be drawn as well.
 	 */
 	private class RadioItemStateListener implements ItemListener {
 
 		@Override
 		public void itemStateChanged(ItemEvent event) {
-			
+
 			// Don't update when deselected or highlighted...
-			if (event.getStateChange() == ItemEvent.SELECTED) {
+			if(event.getStateChange() == ItemEvent.SELECTED) {
 				updatePreview();
 			}
 		}
-		
+
 	}
-	
+
 	/**
 	 * Whenever the selection state of any GUI component which has this listener
 	 * attached will be changed, it causes the export preview to update.
-	 * For example, if the user checks the 'Show selections' JCheckBox, 
+	 * For example, if the user checks the 'Show selections' JCheckBox,
 	 * the update will invoke the recreation of the preview components so
-	 * that selections will be drawn as well. 
+	 * that selections will be drawn as well.
 	 */
 	private class CheckItemStateListener implements ItemListener {
 
 		@Override
 		public void itemStateChanged(ItemEvent event) {
-			
+
 			// Don't update when highlighted...
-			if (event.getStateChange() == ItemEvent.SELECTED
-					|| event.getStateChange() == ItemEvent.DESELECTED) {
+			if((event.getStateChange() == ItemEvent.SELECTED)
+				|| (event.getStateChange() == ItemEvent.DESELECTED)) {
 				updatePreview();
 			}
 		}
-		
+
 	}
-	
+
 	/**
 	 * Update the preview panel of the export dialog according to selected
 	 * options.
 	 */
 	private void updatePreview() {
-		
+
 		if(exportDialog == null) {
 			LogBuffer.println("No exportDialog object defined. Could "
-					+ "not update preview components.");
+				+ "not update preview components.");
 			return;
 		}
-		
+
 		exportDialog.retrieveOptions(exportOptions);
 		setNewPreviewComponents(exportOptions);
-		
+
 		exportDialog.updatePreviewComponents(exportOptions);
 	}
 }
