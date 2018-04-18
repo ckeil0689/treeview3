4284783ac5fa9d03e0a43912d709dfea394c930f
hepcat72 <hepcat72@gmail.com>
2018-04-18 14:44:42 -0400

    Changed macSetup to osSetup.

--- src/main/java/edu/stanford/genetics/treeview/app/TreeView3.java
+++ src/main/java/edu/stanford/genetics/treeview/app/TreeView3.java
@@ -19,36 +19,37 @@ public class TreeView3 {
 	 * @return true if the operating system on which the JVM runs is OSX
 	 */
 	public static boolean isMac() {
-		
+
 		final String os = System.getProperty("os.name").toLowerCase();
 		final boolean isMac = os.startsWith("mac os x");
-		
+
 		return isMac;
 	}
-	
+
 	/**
 	 * Sets up important system properties for Apple OS X systems. This includes
 	 * mostly properties related to the OS X menu bar.
 	 */
-	private static void macSetup() {
+	private static void osSetup() {
 
-		if (!isMac()) return;
+		if(isMac()) {
 
-		LogBuffer.println("Running on a Mac.");
+			LogBuffer.println("Running on a Mac.");
 
-		System.setProperty("apple.laf.useScreenMenuBar", "true");
-		//This does not appear to do anything (on mac)
-		//System.setProperty("com.apple.mrj.application.apple.menu.about.name",
-		//	"TreeView3");
-		//The latest Mac OS X doesn't have growboxes, so this is here primarily
-		//for older versions of OS X
-		System.setProperty("apple.awt.showGrowBox", "true");
+			System.setProperty("apple.laf.useScreenMenuBar", "true");
+			//This does not appear to do anything (on mac)
+			//System.setProperty("com.apple.mrj.application.apple.menu.about.name",
+			//	"TreeView3");
+			//The latest Mac OS X doesn't have growboxes, so this is here primarily
+			//for older versions of OS X
+			System.setProperty("apple.awt.showGrowBox", "true");
+		}
 	}
 
 	public static void main(final String[] args) {
 
 		try {
-			macSetup();
+			osSetup();
 
 			/*
 			 * Set the look and feel to the system look and feel of the user's
