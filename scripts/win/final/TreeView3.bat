c92e0253b697475a55159b2e28798003ffc2d82e
Christopher Keil <ckeil0689@gmail.com>
2020-05-10 13:25:00 +0200

    new .bat file

--- scripts/win/final/TreeView3.bat
+++ scripts/win/final/TreeView3.bat
@@ -1,15 +1,15 @@
 
 @echo off
-"%JAVA_HOME%\bin\java.exe" -Xmx4g -jar "treeview3-all-b58a7883.jar"
+"%JAVA_HOME%\bin\java.exe" -Xmx4g -jar "treeview3-all-0fd0d0b3.jar"
 
-if ERRORLEVEL 1 "%JAVA_HOME%\bin\java.exe" -Xmx2g -jar "treeview3-all-b58a7883.jar"
+if ERRORLEVEL 1 "%JAVA_HOME%\bin\java.exe" -Xmx2g -jar "treeview3-all-0fd0d0b3.jar"
 
-if ERRORLEVEL 1 "%JAVA_HOME%\bin\java.exe" -Xmx1g -jar "treeview3-all-b58a7883.jar"
+if ERRORLEVEL 1 "%JAVA_HOME%\bin\java.exe" -Xmx1g -jar "treeview3-all-0fd0d0b3.jar"
 
-if ERRORLEVEL 1 java.exe -Xmx4g -jar "treeview3-all-b58a7883.jar"
+if ERRORLEVEL 1 java.exe -Xmx4g -jar "treeview3-all-0fd0d0b3.jar"
 
-if ERRORLEVEL 1 java.exe -Xmx2g -jar "treeview3-all-b58a7883.jar"
+if ERRORLEVEL 1 java.exe -Xmx2g -jar "treeview3-all-0fd0d0b3.jar"
 
-if ERRORLEVEL 1 java.exe -Xmx1g -jar "treeview3-all-b58a7883.jar"
+if ERRORLEVEL 1 java.exe -Xmx1g -jar "treeview3-all-0fd0d0b3.jar"
 
 if ERRORLEVEL 1 start "" /wait cmd /c "echo Error starting Java VM.  Please make sure you have at least java 1.7 installed.&echo(&pause"
